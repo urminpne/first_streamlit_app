bfe2b08b744450a01070935cb979a14183b19fc6
Urmila Neupane <112533883+urminpne@users.noreply.github.com>
2022-08-31 00:54:47 -0400

    Update streamlit_app.py
    added new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Freee_Rabge Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
-
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 
